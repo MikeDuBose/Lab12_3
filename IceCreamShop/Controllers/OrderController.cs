cf20ed0594b738c3f5af7cff38d440871ec4d2ca
Michael DuBose <michaeldubose@quickenloans.com>
2020-04-06 08:36:28 -0400

    Added extra fields that are validated via server (?)

--- IceCreamShop/Controllers/OrderController.cs
+++ IceCreamShop/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IceCreamShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IceCreamShop.Controllers
@@ -14,12 +15,29 @@ namespace IceCreamShop.Controllers
             return View("OrderIndex");
         }
 
-        public IActionResult Order(string fname, string lname, string email, string password)
+        public IActionResult Order(string FirstName, string LastName, string email, string phoneNum, string age, string options, string password)
         {
-            ViewData["name"] = fname + " " + lname;
+            ViewData["FirstName"] = FirstName;
+            ViewData["LastName"] = LastName;
             ViewData["email"] = email;
+            ViewData["phoneNum"] = phoneNum;
+            ViewData["age"] = age;
+            ViewData["options"] = options;
             ViewData["password"] = password;
             return View();
+        }
+
+        public IActionResult UserModel(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(user);
+            }
+            else
+            {
+                ViewData["errorMsg"] = "Your form had errors. Please correct and re-submit.";
+                return View("Dish", user);
+            }
         }
     }
 }
---
+++ IceCreamShop/Models/UserModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace IceCreamShop.Models
+{
+    public class UserModel
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phoneNum;
+        private string age;
+        private string options;
+        private string password;
+
+        [DisplayName("FirstName")]
+        [Required]
+        [MaxLength(20)]
+        [MinLength(2)]
+        public string FirstName { get => firstName; set => firstName = value; }
+
+        [DisplayName("LastName")]
+        [Required]
+        [MaxLength(20)]
+        [MinLength(2)]
+        public string LastName { get => lastName; set => lastName = value; }
+
+        [DisplayName("E-mail")]
+        [EmailAddress]
+        [Required]
+        public string Email { get => email; set => email = value; }
+
+        [DisplayName("Phone Number")]
+        [Required]
+        public string PhoneNum { get => phoneNum; set => phoneNum = value; }
+
+        [DisplayName("Age")]
+        [Required]
+        public string Age { get => age; set => age = value; }
+
+        [DisplayName("Password")]
+        [Required]
+        public string Password { get => password; set => password = value; }
+
+        [DisplayName("Options")]
+        public string Options { get => options; set => options = value; }
+    }
+}
+
