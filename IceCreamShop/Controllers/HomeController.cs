88fccb4cffa3c6085df379fe2f880aab0e31dc47
Michael DuBose <michaeldubose@quickenloans.com>
2020-04-08 08:36:43 -0400

    SQL Server added

--- IceCreamShop/Controllers/HomeController.cs
+++ IceCreamShop/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IceCreamShop.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace IceCreamShop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        IConfiguration ConfigRoot;
+        SqlConnection connection;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration config)
         {
-            _logger = logger;
+            ConfigRoot = config;
+            connection = new SqlConnection(ConfigRoot.GetConnectionString("CoffeeShopDB"));
         }
 
         public IActionResult Index()
@@ -30,11 +35,16 @@ namespace IceCreamShop.Controllers
 
         public IActionResult Menu()
         {
-            string[] flavors = { "Brewed", "Cold Brew", "Au Lait", "Double Espresso", "Americano", "Cortado", "Cappucino", "Latte", "Mocha", "Frappe", "Beans by the pound", 
-            "Iced Tea", "Hot Tea", "Chai Tea", "Chai Latte", "Matcha Latte", "Hot Chocolate"};
-            string[] prices = { "1.99", "2.99", "3.50", "5.00", "3.50", "3.50", "4.00", "6.00", "5.00", "4.50", "20.00", "3.50", "3.50", "4.50", "4.50", "5.00", "2.00"};
-            ViewData["flavors"] = flavors;
-            ViewData["prices"] = prices;
+            string queryString = "SELECT * FROM Products";
+            var Products = connection.Query<Product>(queryString); //queries the product class for the string declared above
+            ViewData["Products"] = Products;
+
+            //string[] flavors = { "Brewed", "Cold Brew", "Au Lait", "Double Espresso", "Americano", "Cortado", "Cappucino", "Latte", "Mocha", "Frappe", "Beans by the pound", 
+            //"Iced Tea", "Hot Tea", "Chai Tea", "Chai Latte", "Matcha Latte", "Hot Chocolate"};
+            //string[] prices = { "1.99", "2.99", "3.50", "5.00", "3.50", "3.50", "4.00", "6.00", "5.00", "4.50", "20.00", "3.50", "3.50", "4.50", "4.50", "5.00", "2.00"};
+            //ViewData["flavors"] = flavors;
+            //ViewData["prices"] = prices;
+
             return View();
         }
 
